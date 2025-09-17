fadd56f8d1aef3877c5c6ff345b7ee8e3889eab6
56017286 <transcribing24@gmail.com>
2025-09-17 20:27:15 +0200

    The order program first attempt, still need help with proper;ly facilitating the Display order part. How do I do That?

---
+++ OrderProgram/OrderProgram/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace OrderProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayOrderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAddQuant = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubQuant = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.displayOrderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(65, 21);
+            this.processToolStripMenuItem.Text = "Process";
+            this.processToolStripMenuItem.Click += new System.EventHandler(this.ProcessOrderClick);
+            // 
+            // displayOrderToolStripMenuItem
+            // 
+            this.displayOrderToolStripMenuItem.Name = "displayOrderToolStripMenuItem";
+            this.displayOrderToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.displayOrderToolStripMenuItem.Text = "Display Order";
+            this.displayOrderToolStripMenuItem.Click += new System.EventHandler(this.DisplayorderClick);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitAppClick);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.HelpClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Jersey Size";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "XSmall",
+            "Small",
+            "Medium",
+            "Large",
+            "XLarge",
+            "XXLarge"});
+            this.comboBox1.Location = new System.Drawing.Point(197, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Tag = "XSmall";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select Quantity of Jerseys";
+            // 
+            // btnAddQuant
+            // 
+            this.btnAddQuant.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddQuant.Location = new System.Drawing.Point(180, 223);
+            this.btnAddQuant.Name = "btnAddQuant";
+            this.btnAddQuant.Size = new System.Drawing.Size(75, 58);
+            this.btnAddQuant.TabIndex = 4;
+            this.btnAddQuant.Text = "+";
+            this.btnAddQuant.UseVisualStyleBackColor = true;
+            this.btnAddQuant.Click += new System.EventHandler(this.BtnAddClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // btnSubQuant
+            // 
+            this.btnSubQuant.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubQuant.Location = new System.Drawing.Point(261, 223);
+            this.btnSubQuant.Name = "btnSubQuant";
+            this.btnSubQuant.Size = new System.Drawing.Size(75, 58);
+            this.btnSubQuant.TabIndex = 6;
+            this.btnSubQuant.Text = "-";
+            this.btnSubQuant.UseVisualStyleBackColor = true;
+            this.btnSubQuant.Click += new System.EventHandler(this.SubQuantClik);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(510, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(261, 277);
+            this.listBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(316, 398);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Subtotal";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(390, 47);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add to Cart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.AddToCartClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(316, 428);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Total incl. VAT 14%:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSubQuant);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAddQuant);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem displayOrderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddQuant;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubQuant;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ OrderProgram/OrderProgram/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int quantity = 0;
+        double subTot = 0;
+        double total = 0;
+
+        private void ProcessOrderClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayorderClick(object sender, EventArgs e)
+        {
+            total += subTot + 0.14 * subTot;
+
+            label5.Text = $"Total incl. VAT 14%: R{total}";
+        }
+
+        private void ExitAppClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void HelpClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("First select the size of jersey that you need.\n\n Select the quantity of Jerseys you need. To add jerseys, click on the plus button." +
+                "To decrease the number of jersey, click on the minus button. The quantity of Jerseys chosen will be displayed above the buttons. Make sure the " +
+                "quantity is greater than zero.\n\nClick on the green Add to Cart button to add the item to the cart.\n\n" +
+                "The total displayed at the bottom exludes the 14% VAT.\n\nTo display your Cart plus the total owed with VAT, under Process, click on Display Order.");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubQuantClik(object sender, EventArgs e)
+        {
+            quantity -= 1;
+
+            label3.Text = $"{quantity}";
+        }
+
+        private void BtnAddClick(object sender, EventArgs e)
+        {
+            quantity += 1;
+            label3.Text = $"{quantity}";
+
+        }
+
+        private void AddToCartClick(object sender, EventArgs e)
+        {
+            string selSize = comboBox1.SelectedItem.ToString();
+
+            if(quantity > 1)
+            {
+                listBox1.Items.Add($"{quantity} {selSize} Jerseys");
+            }
+            else if(quantity == 1)
+            {
+                listBox1.Items.Add($"{quantity} {selSize} Jersey");
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid quantity of jerseys. Quantity selected must be greater than or equal 1");
+                return;
+            }
+
+
+            if(selSize == "XSmall" || selSize == "XXLarge")
+            {
+                subTot += quantity * 20;
+            }
+
+            subTot += quantity * 16;
+
+            label4.Text = $"Subtotal: R{subTot}";
+
+
+        }
+    }
+}
